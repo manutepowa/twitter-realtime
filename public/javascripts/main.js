c32980e80dd7298e1dfba802d58ab03c9a0c1543
Manuel <manutepowa@gmail.com>
2016-03-17 20:13:39 +0100

    Side bar rehecho

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,34 @@
-$(document).ready(function(){
-	$("#menu-toggle").click(function(e) {
+$(document).ready(function() {
+    var aORr = true;
+    $(".sidebar-footer").click(function(e) {
         e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
+        // // $("#wrapper").toggleClass("toggled");
+        // console.log(aORr);
+
+        if (aORr) {
+            $("#wrapper").addClass("toggled");
+            $("#logo2").css({
+                visibility: 'hidden'
+            });
+            $("#logo1").css({
+                display: 'inline'
+            });
+            $(".sidebar-footer i").css({
+                transform: 'rotate(180deg)',
+                transition: '.2s'
+            });
+            aORr = false;
+        } else {
+            $(".sidebar-footer i").css({
+                transform: 'rotate(360deg)',
+                transition: '.2s'
+            });
+            $("#wrapper").removeClass("toggled");
+            $("#logo2").css({
+                visibility: 'visible'
+            });
+            aORr = true;
+        }
+
     });
 })
-
-
-
