c7ce5a7fc53791d2ef0fbf928f5e2f6821c6d44d
Manuel <manutepowa@gmail.com>
2016-04-07 19:23:20 +0200

    ..

--- config/configTwitter.js
+++ config/configTwitter.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: 'K7uV5g0WPbd4RtLIJfj9uHjXt'
-  , consumer_secret: 'QVvtA2MQoHUXQFi2ZFtzyIt6iuIEhRJTYVY5FPcdzu2ZPa8QGz'
+    consumer_key: 'tPSmO3BvyvntxEtLh6x9o024l'
+  , consumer_secret: 'LkpgpKkBR3AeXakmyvAG6l32PeqTzIokK0kfMkWdTf7DvFK2VS'
   , access_token: '2984707788-GfTFGvJCrLRlX2qb1CPkbsPIBYONpSx99DkQljm'
   , access_token_secret: 'i8dSnXuSHOlfwUjlLKhzVGUN7j9uEf9nt9IHkIStnJKlz'
 }
